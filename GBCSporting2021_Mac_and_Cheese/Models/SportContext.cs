be66e81b363d5d19ee8549d3b41c08ef8a1ab9c1
saloni <saloniprajapati024@gmail.com>
2021-02-23 16:05:49 -0600

    database

---
+++ GBCSporting2021_Mac_and_Cheese/Models/SportContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GBCSporting2021_Mac_and_Cheese.Models
+{
+    public class SportContext:DbContext
+    {
+        public SportContext(DbContextOptions<SportContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Incident> Incidents { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Technician> Technicians { get; set; }
+    }
+}
