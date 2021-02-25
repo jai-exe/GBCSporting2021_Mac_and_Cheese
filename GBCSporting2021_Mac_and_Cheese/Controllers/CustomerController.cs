b43315eee44bc015d635fab15dd14e374ae92169
saloni <saloniprajapati024@gmail.com>
2021-02-24 20:22:57 -0600

    customer

--- GBCSporting2021_Mac_and_Cheese/Controllers/CustomerController.cs
+++ GBCSporting2021_Mac_and_Cheese/Controllers/CustomerController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GBCSporting2021_Mac_and_Cheese.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,24 +10,78 @@ namespace GBCSporting2021_Mac_and_Cheese.Controllers
 {
     public class CustomerController : Controller
     {
+        private ContactContext context { get; set; }
+
+        public CustomerController(ContactContext ctx)
+        {
+            context = ctx;
+        }
+        [HttpGet]
         public IActionResult List()
         {
-            return View();
+            var customers = context.Customers
+                               .Include(c => c.Country)
+                               .OrderBy(c => c.CustFName).ToList();
+            return View(customers);
         }
-
+        [HttpGet]
         public IActionResult Add()
         {
-            return View("Edit");
+            ViewBag.Action = "Add";
+            ViewBag.Countries = context.Countries.OrderBy(c => c.CountryName).ToList();
+            return View("Edit", new Customer());
         }
-
-        public IActionResult Edit()
+        [HttpGet]
+        public IActionResult Edit(int id)
         {
-            return View();
+            var customers = context.Customers
+                               .Include(c => c.Country)
+                               .FirstOrDefault(c => c.CustId == id);
+
+            ViewBag.Action = "Edit";
+            ViewBag.Countries = context.Countries.OrderBy(c => c.CountryName).ToList();
+            return View(customers);
         }
-
-        public IActionResult Delete()
+        [HttpPost]
+        public IActionResult Edit(Customer customer)
         {
-            return View();
+            string action = (customer.CustId == 0) ? "Add" : "Edit";
+            if (ModelState.IsValid)
+            {
+                if(action == "Add")
+                {
+                    context.Customers.Add(customer);
+                }
+                else
+                {
+                    context.Customers.Update(customer);
+                }
+                context.SaveChanges();
+                return RedirectToAction("List", "Customer");
+            }
+            else
+            {
+                ViewBag.Action = action;
+                ViewBag.Countries = context.Countries.OrderBy(c => c.CountryName).ToList();
+                return View(customer);
+            }
+           
+
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var customers = context.Customers
+                               .Include(c => c.Country)
+                               .FirstOrDefault(c => c.CustId == id);
+            return View(customers);
+        }
+        [HttpPost]
+        public IActionResult Delete(Customer customer)
+        {
+            context.Customers.Remove(customer);
+            context.SaveChanges();
+            return RedirectToAction("List","Customer");
         }
     }
 }
--- GBCSporting2021_Mac_and_Cheese/Controllers/HomeController.cs
+++ GBCSporting2021_Mac_and_Cheese/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using GBCSporting2021_Mac_and_Cheese.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -11,6 +12,12 @@ namespace GBCSporting2021_Mac_and_Cheese.Controllers
 {
     public class HomeController : Controller
     {
+        private ContactContext context { get; set; }
+
+        public HomeController(ContactContext ctx)
+        {
+            context = ctx;
+        }
         public IActionResult Index()
         {
             return View();
